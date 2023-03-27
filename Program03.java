2e724db6d84ebe18bec9c0456d7a0955165fb614
ismartviswa <128114792+ismartviswa@users.noreply.github.com>
2023-03-27 10:38:38 +0530

    Add files via upload

---
+++ Program03.java
@@ -0,0 +1,15 @@
+import java.until.Scanner
+class Program04
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your name:");
+String sn=sc.next();
+System.out.println("\nGood Evening "+sn);
+System.out.println("\nEnter two numbers:");
+int num1=sc.nextint();
+int num2=sc.nexrint();
+System.out.println("\n"+num1+"*"+num2+"="+num1*num2);
+}
+}
