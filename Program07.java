044d71c399af61fc4402363130436fdc0211cef4
ismartviswa <128114792+ismartviswa@users.noreply.github.com>
2023-03-27 10:41:06 +0530

    Add files via upload

---
+++ Program07.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Program07
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number:");
+int num1=sc.nextInt();
+System.out.println("\n"+num1+"*"+0+"="+num1*0);
+System.out.println("\n"+num1+"*"+1+"="+num1*1);
+System.out.println("\n"+num1+"*"+2+"="+num1*2);
+System.out.println("\n"+num1+"*"+3+"="+num1*3);
+System.out.println("\n"+num1+"*"+4+"="+num1*4);
+System.out.println("\n"+num1+"*"+5+"="+num1*5);
+System.out.println("\n"+num1+"*"+6+"="+num1*6);
+System.out.println("\n"+num1+"*"+7+"="+num1*7);
+System.out.println("\n"+num1+"*"+8+"="+num1*8);
+System.out.println("\n"+num1+"*"+9+"="+num1*9);
+System.out.println("\n"+num1+"*"+10+"="+num1*10);
+System.out.println("\n"+num1+"*"+11+"="+num1*11);
+System.out.println("\n"+num1+"*"+12+"="+num1*12);
+System.out.println("\n"+num1+"*"+13+"="+num1*13);
+System.out.println("\n"+num1+"*"+14+"="+num1*14);
+System.out.println("\n"+num1+"*"+15+"="+num1*15);
+System.out.println("\n"+num1+"*"+16+"="+num1*16);
+System.out.println("\n"+num1+"*"+17+"="+num1*17);
+System.out.println("\n"+num1+"*"+18+"="+num1*18);
+System.out.println("\n"+num1+"*"+19+"="+num1*19);
+System.out.println("\n"+num1+"*"+20+"="+num1*20);
+}
+}
